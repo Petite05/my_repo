cac212e0a32ace37c7f20fd9047c4c6fc0e15ad4
Petite05 <122143047+Petite05@users.noreply.github.com>
2024-03-22 01:33:23 +0100

    Add files via upload

---
+++ #1_type_casting.py
@@ -0,0 +1,32 @@
+
+# We have two different types of Data: they are; [ Implicit conversion and Explicit conversion]:
+
+# Implicit conversion data is performed automatic by Python compiler: And down hereis the exmp of Explicit conversion which's converted by Developers:
+
+# Casting in python is a process use to convert one data type to another Data:
+
+
+
+x = 20
+y = 'Hello world'
+print(str(x))
+print(int(x))
+# print(ord()):
+print(hex(x))
+print(oct(x))
+print(tuple(y))
+print(set(y))
+print(list(y))
+# print(dict())
+
+
+
+print("Hello world!"); print("Hello world!"); print("Hello") ; print('World')    # we canb use [;] to code different codes in a single line:
+
+x          =                1                     +                     2
+print(x)
+
+c,v,n = 10,2,4
+print(c)
+print(v)
+print(n)
