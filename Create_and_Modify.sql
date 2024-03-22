78badc799a55223c84cf5b4c51a53cbf0ef2f9ab
Petite05 <122143047+Petite05@users.noreply.github.com>
2024-03-22 01:52:09 +0100

    Create Create_and_Modify.sql

---
+++ Create_and_Modify.sql
@@ -0,0 +1,26 @@
+
+
+CREATE A DATABASED;
+
+CREATE DATABASE school;
+
+# ALTER IS USED TO MODIFY;
+  
+CREATE TABLE cm_mobiles (username VARCHAR(50), fullname CHAR(7), email VARCHAR(80)); # this is used for my practice but let m save it here:
+
+ALTER TABLE school_table ADD (age INT, country VARCHAR(50), gender VARCHAR(20), nationality VARchar(255));
+ALTER TABLE school_table MODIFY country VARCHAR(100); # HERE WE UPDATE VARCHAR(FROM 50 TO 100)
+
+CREATE TABLE school_db.student_player (school VARCHAR(50), student_player_id INT);
+
+
+# CREATE A TABLE THAT HAVE ALL THE INFORMATION ABOUT CUSTOMERS:
+
+CREATE TABLE customers (CustomerId int, FirstName VARCHAR(50), LastName VARCHAR(40),
+Company VARCHAR(80), Address VARCHAR(60), City VARCHAR(40), State VARCHAR(40),
+Country VARCHAR(50), PostalCode VARCHAR(10), Phone VARCHAR(15), Fax VARCHAR(25),
+Email VARCHAR(100), SupportRid INT);
+
+
+CREATE TABLE student_player (school VARCHAR(50), student_player_id INT);
+
